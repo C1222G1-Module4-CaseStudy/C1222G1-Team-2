acd9c44c6aaf4796e2e8047df33cca15a2035ec0
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-05-25 21:46:50 +0700

    commit

--- codegym-foods/src/main/java/com/example/codegymfoods/controller/HomeController.java
+++ codegym-foods/src/main/java/com/example/codegymfoods/controller/HomeController.java
@@ -1,14 +1,38 @@
 package com.example.codegymfoods.controller;
 
+import com.example.codegymfoods.model.product.Product;
+import com.example.codegymfoods.model.product.ProductType;
+import com.example.codegymfoods.service.blog.IBlogService;
+import com.example.codegymfoods.service.product.IProductService;
+import com.example.codegymfoods.service.product.IProductTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/index")
+@RequestMapping("/home")
 public class HomeController {
+    @Autowired
+    private IBlogService iBlogService;
+    @Autowired
+    private IProductService productService;
+    @Autowired
+    private IProductTypeService productTypeService;
     @GetMapping("")
-    public String index() {
+    public String index(Model model,
+                        @PageableDefault(size = 3) Pageable pageable) {
+        model.addAttribute("blogList", this.iBlogService.getBlog(pageable));
+        Page<Product> productList = productService.getBlogPage(pageable);
+        List<ProductType> productTypeList = productTypeService.getAll();
+        model.addAttribute("productTypeList", productTypeList);
+        model.addAttribute("productList", productList);
         return "index";
     }
 }
