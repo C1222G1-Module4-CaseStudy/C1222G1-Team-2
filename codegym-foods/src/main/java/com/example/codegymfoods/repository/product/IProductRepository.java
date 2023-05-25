01786312037ecb64e793281581c34d9a07521706
tienhung79 <ngotienhung2000@gmail.com>
2023-05-25 10:41:55 +0700

    up

---
+++ codegym-foods/src/main/java/com/example/codegymfoods/repository/product/IProductRepository.java
@@ -0,0 +1,14 @@
+package com.example.codegymfoods.repository.product;
+
+import com.example.codegymfoods.model.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IProductRepository extends JpaRepository<Product,Integer> {
+    @Query(value = "select * from product join product_type on product.product_type_id = product_type.id where product_type.id = ?",nativeQuery = true)
+    List<Product> findAllByType(int id);
+}
